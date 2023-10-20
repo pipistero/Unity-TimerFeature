5b1f205882063707c79bc2326a9c1410e1d7fc95
Anton Kazakevich <laweirdcompany@gmail.com>
2023-10-20 05:27:56 +0300

    TimerFeature done; TODO: documentation; readme; release

---
+++ Assets/PS/TimerFeature/Timer/Timer.cs
@@ -0,0 +1,154 @@
+using System;
+using PS.TimerFeature.Types;
+using UnityEngine;
+
+namespace PS.TimerFeature.Timer
+{
+    public class Timer
+    {
+        public event Action<float> TimerValueChanged;
+        public event Action TimerFinished;
+        public event Action TimerPaused;
+        public event Action TimerUnpaused;
+        
+        public TimerType Type { get; }
+        public float RemainingTime { get; private set; }
+        public bool IsPaused { get; private set; }
+        
+        public Timer(TimerType type, float duration)
+        {
+            Type = type;
+            SetRemainingTime(duration);
+        }
+        
+        public void SetRemainingTime(float remainingTime)
+        {
+            RemainingTime = remainingTime;
+            TimerValueChanged?.Invoke(RemainingTime);
+        }
+
+        public void Start()
+        {
+            if (RemainingTime <= 0f)
+            {
+                Debug.Log($"Timer ({Type}) already finished");
+                return;
+            }
+            
+            IsPaused = false;
+            
+            SubscribeEvents();
+            
+            TimerValueChanged?.Invoke(RemainingTime);
+        }
+
+        public void Start(float duration)
+        {
+            SetRemainingTime(duration);
+            Start();
+        }
+
+        public void Pause()
+        {
+            IsPaused = true;
+            
+            UnsubscribeEvents();
+            
+            TimerPaused?.Invoke();
+        }
+
+        public void Unpause()
+        {
+            IsPaused = false;
+            
+            Start();
+            
+            TimerUnpaused?.Invoke();
+        }
+
+        public void Stop()
+        {
+            IsPaused = false;
+            
+            SetRemainingTime(0f);
+            
+            TimerFinished?.Invoke();
+        }
+
+        #region Internal events work
+
+        private void SubscribeEvents()
+        {
+            switch (Type)
+            {
+                case TimerType.SecondTick:
+                    TimeInvoker.TimeInvoker.Instance.SecondTicked += OnSecondTimerTicked;
+                    break;
+                
+                case TimerType.UpdateTick:
+                    TimeInvoker.TimeInvoker.Instance.UpdateTicked += OnUpdateTimerTicked;
+                    break;
+                
+                case TimerType.SecondTickUnscaled:
+                    TimeInvoker.TimeInvoker.Instance.SecondTickedUnscaled += OnSecondTimerTicked;
+                    break;
+                
+                case TimerType.UpdateTickUnscaled:
+                    TimeInvoker.TimeInvoker.Instance.UpdateTickedUnscaled += OnUpdateTimerTicked;
+                    break;
+            }
+        }
+
+        private void UnsubscribeEvents()
+        {
+            switch (Type)
+            {
+                case TimerType.SecondTick:
+                    TimeInvoker.TimeInvoker.Instance.SecondTicked -= OnSecondTimerTicked;
+                    break;
+                
+                case TimerType.UpdateTick:
+                    TimeInvoker.TimeInvoker.Instance.UpdateTicked -= OnUpdateTimerTicked;
+                    break;
+                
+                case TimerType.SecondTickUnscaled:
+                    TimeInvoker.TimeInvoker.Instance.SecondTickedUnscaled -= OnSecondTimerTicked;
+                    break;
+                
+                case TimerType.UpdateTickUnscaled:
+                    TimeInvoker.TimeInvoker.Instance.UpdateTickedUnscaled -= OnUpdateTimerTicked;
+                    break;
+            }
+        }
+
+        #endregion
+
+        #region Internal timer work
+
+        private void OnUpdateTimerTicked(float deltaTime)
+        {
+            if (IsPaused)
+                return;
+
+            SetRemainingTime(RemainingTime -= deltaTime);
+            CheckFinish();
+        }
+
+        private void OnSecondTimerTicked()
+        {
+            if (IsPaused)
+                return;
+            
+            SetRemainingTime(RemainingTime -= 1f);
+            CheckFinish();
+        }
+
+        private void CheckFinish()
+        {
+            if (RemainingTime <= 0f)
+                Stop();
+        }
+
+        #endregion
+    }
+}
